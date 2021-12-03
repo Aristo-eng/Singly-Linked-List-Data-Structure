5543be9249d35867bb5a53b51e2da48e84eb8304
Aristo-eng <ribeus.munezero@yahoo.com>
2021-12-03 14:22:45 +0200

    Updated ReadMe.md && added insert at end method

--- README.md
+++ README.md
@@ -7,3 +7,21 @@ Linked List is defined as a data structure wherein the elements are not stored a
 ## Features
 - Define class Node
 - Define class SingleLinkedList
+
+## class SingleLinkedList
+
+SingleLinkedList has methods:
+
+- print_linked_list
+```sh
+def print_linked_list(self):
+    # If empty linked list is given, return ""
+    # else: Traversing the given LL and print values till we reach the end
+```
+- insert_at_end
+
+```sh
+def insert_at_end(self, data):
+    # If empty linked list is given, head = new_node
+    # else: Traversing the given LL to reach the end
+```
--- Singly_Linked_List.py
+++ Singly_Linked_List.py
@@ -7,8 +7,40 @@ class SingleLinkedList:
     def __init__(self):
         self.head = None
 
+    def print_linked_list(self):
+        if self.head is None:
+            return ""
+
+        node = self.head
+        while node:
+            print(node.value, end = "   ")
+            node = node.next
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        ref_head = self.head
+        while ref_head.next:
+            ref_head = ref_head.next
+        ref_head.next = new_node 
+
+    
+
+
+
 
 if __name__ == '__main__': 
     # Start with an empty list
-    list = SingleLinkedList()
-    print(type(list))
+    list_s = SingleLinkedList()
+
+    # Insert value at the end.  So linked list becomes value->None
+    list_s.insert_at_end(1)
+    list_s.insert_at_end(2)
+
+
+    # print the linked list
+    list_s.print_linked_list()
